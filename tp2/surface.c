80bfa3227acd5429072fcbe92f3ffc3387152275
Hamza El Meknassi <elmeknassi.h@gmail.com>
2016-02-18 02:31:24 +0100

    Traçage de polygone à partir de liste de points dans un fichier

--- tp2/surface.c
+++ tp2/surface.c
@@ -2,10 +2,26 @@
 #include "ez-image.h"
 #include "listep_op.h"
 
+typedef struct {
+  PLISTE list;
+} Win_Data;
+
 void win1_on_expose(Ez_event *ev) {
+  Win_Data *win1_data = ez_get_data(ev->win);
+  PLISTE list = win1_data->list;
+  EPOINT *dernier_point, *avantdernier_point;
+
   ez_set_color(ez_red);
-  ez_set_thick(1);
-  ez_draw_point(ev->win, 350, 400);
+  ez_set_thick(5);
+
+  avantdernier_point = list;
+  dernier_point = list->next;
+  while (dernier_point != NULL) {
+    ez_draw_line(ev->win, avantdernier_point->x , avantdernier_point->y, dernier_point->x, dernier_point->y);
+    avantdernier_point = dernier_point;
+    dernier_point = dernier_point->next;
+  }
+  ez_draw_line(ev->win, avantdernier_point->x, avantdernier_point->y, list->x, list->y);
 }
 
 void win1_event(Ez_event *ev) {
@@ -17,15 +33,16 @@ void win1_event(Ez_event *ev) {
 }
 
 int main(int argc, char *argv[]) {
-  Ez_window win1;
-  PLISTE mlist;
-
-  mlist = lire_liste("k1.in");
+  Ez_window window;
+  Win_Data win1_data;
+  
+  win1_data.list = lire_liste("k1.in");
 
   if (ez_init() < 0)
     exit(1);
-
-  win1 = ez_window_create(500, 500, "SURFACE", win1_event);
+  
+  window = ez_window_create(500, 500, "SURFACE", win1_event);
+  ez_set_data(window, &win1_data);
   
   ez_main_loop();
   exit(0);

3fa226d889f07c82d09dbdbaf3c1e15b9b123ea6
Hamza El Meknassi <elmeknassi.h@gmail.com>
2016-02-21 04:34:03 +0100

    Mode simultané terminé

--- tp2/animation.c
+++ tp2/animation.c
@@ -1,5 +1,5 @@
+#include <string.h>
 #include "ez-draw.h"
-#include "ez-image.h"
 #include "listep_op.h"
 
 typedef enum {
@@ -9,18 +9,19 @@ typedef enum {
 
 typedef struct {
   PLISTE list[5];
-  Expose_Mode mode = SIMULTANE;
+  Expose_Mode mode;
 } Win_Data;
 
 void win1_on_expose(Ez_event *ev) {
-  Win_Data data = ez_get_data(ev->win);
-  PLISTE list[5] = data->list;
+  Win_Data *data = ez_get_data(ev->win);
+  PLISTE *list = data->list;
   EPOINT *dernier_point, *avantdernier_point;
+  int i;
 
   if (data->mode == SIMULTANE) {
     for (i=0; i<5; i++) { //TODO: Tester la partie SIMULTANE
-      ez_set_color(ez_get_HSV(i*20,1,1));
-      ez_set_thick(5);
+      ez_set_color(ez_get_HSV(i*80,1,1));
+      ez_set_thick(4);
 
       avantdernier_point = list[i];
       dernier_point = list[i]->next;
@@ -29,7 +30,7 @@ void win1_on_expose(Ez_event *ev) {
 	avantdernier_point = dernier_point;
 	dernier_point = dernier_point->next;
       }
-      ez_draw_line(ev->win, avantdernier_point->x, avantdernier_point->y, list->x, list->y);
+      ez_draw_line(ev->win, avantdernier_point->x, avantdernier_point->y, list[i]->x, list[i]->y);
     }
   } else if (data->mode == SEQUENTIEL) {
     //TODO: Implémenter partie affichant listes à intervalle t
@@ -37,21 +38,27 @@ void win1_on_expose(Ez_event *ev) {
 }
 
 void win1_event(Ez_event *ev) {
+  Win_Data *data;
   switch(ev->type) {
   case Expose: 
     win1_on_expose(ev);
     break;
   case KeyPress:
     switch (ev->key_sym) {
-    case XK_q : 
+    case XK_q : //Touche Q : Quitte
       ez_quit (); 
       break;
-    case XK_s :
-	Win_Data *win1_data = ez_get_data(ev->win);
-	win1_data->list = lire_liste("");
-	break;
-    case XK_d :
-      
+    case XK_s : //Touche S : Mode simultané
+      data = ez_get_data(ev->win);
+      data->mode = SIMULTANE;
+      ez_set_data(ev->win, data);
+      printf("Mode SIM (%d)\n", data->mode);
+      break;
+    case XK_d : //Touche D : Mode séquentiel
+      data = ez_get_data(ev->win);
+      data->mode = SEQUENTIEL;
+      ez_set_data(ev->win, data);
+      printf("Mode SEQ (%d)\n", data->mode);
     }
     break;
   }
@@ -61,20 +68,28 @@ int main(int argc, char *argv[]) {
   Ez_window window;
   Win_Data data;
   int i;
-  char *nf;
+  char ia[2]; //version char de i
+  char extension[]=".in";
+  char nf[10];
 
-  strcpy(nf, "k0.out");
-      
+  //Initialise nome de fichier à k0.in
+  //strcpy(nf, "k0.in");
+  //Charge en mémoire les fichier k[0-4].in
   for (i=0; i<5; i++) {
-    nf[1] = (char)i;
+    sprintf(ia, "%i", i); //Conversion de i en char
+    strcpy(nf, "k"); //Initialise nf à "k"
+    strcat(nf, ia); //Concatène [0-4]
+    strcat(nf, extension); //Concatène ".in"
+    printf("Chargement du fichier %s...\n", nf);
     data.list[i] = lire_liste(nf);
   }
+  data.mode = SIMULTANE;
 
   if (ez_init() < 0)
     exit(1);
   
   window = ez_window_create(500, 500, "SURFACE", win1_event);
-  ez_set_data(window, &win1_data);
+  ez_set_data(window, &data);
   
   ez_main_loop();
   exit(0);
